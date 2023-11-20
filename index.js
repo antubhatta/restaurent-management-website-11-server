5cfc53e1b4ee013c6bd9291538d3b37060a9b1d5
antubhatta <antubhatta09@gmail.com>
2023-11-21 03:04:51 +0600

    implemented api for food get

--- index.js
+++ index.js
@@ -28,36 +28,129 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    const foodsCollection=client.db('restaurentManagement').collection('foods');
-
-    app.get('/foods',async(req,res)=>{
-      const cursor= foodsCollection.find();
-      const result= await cursor.toArray();
-      res.send(result)
-  })
-
-  app.get('/foods/:id', async(req,res)=>{
-    const id= req.params.id;
-    const query= {_id: new ObjectId(id)}
-    const result= await foodsCollection.findOne(query)
-    res.send(result)
-})
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-app.get('/',(req,res)=>{
-    res.send('Restaurent website is running')
-})
-
-app.listen(port, ()=>{
-    console.log(`Restaurent management website server is running on port: ${port}`)
-})
+    const express = require("express");
+    const cors = require("cors");
+    const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+    require("dotenv").config();
+    const cookieParser = require("cookie-parser");
+    const app = express();
+    const port = process.env.PORT || 5000;
+    
+    // middleware
+    app.use(
+      cors({
+        origin: ["http://localhost:5173"],
+        credentials: true,
+      })
+    );
+    app.use(express.json());
+    app.use(cookieParser());
+    
+    const verify = (req, res, next) => {
+      const token = req?.cookies?.token;
+      // console.log('token in the middleware', token);
+      // no token available
+      if (!token) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.user = decoded;
+        next();
+      });
+    };
+    
+    // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgdhjcn.mongodb.net/?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgdhjcn.mongodb.net/?retryWrites=true&w=majority`;
+    
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+    
+    async function run() {
+      try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+    
+        // Authentication
+        // auth related api
+        app.post("/jwt", async (req, res) => {
+          const user = req.body;
+          console.log("user for token", user);
+          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "1h",
+          });
+    
+          res
+            .cookie("token", token, {
+              httpOnly: true,
+              secure: true,
+              sameSite: "none",
+            })
+            .send({ success: true });
+        });
+    
+        app.post("/logout", async (req, res) => {
+          const user = req.body;
+          console.log("logging out", user);
+          res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+        });
+    
+        const foodsCollection = client
+          .db("restaurentManagement")
+          .collection("foods");
+    
+        app.get("/foods", async (req, res) => {
+          const page = parseInt(req.query.page) || 1;
+          const size = parseInt(req.query.size) || 10;
+    
+          const result = await foodsCollection
+            .find()
+            .skip((page - 1) * size)
+            .limit(size)
+            .toArray();
+    
+          res.send(result);
+        });
+    
+        app.get("/count/foods", async (req, res) => {
+          const result = await foodsCollection.countDocuments({});
+          res.status(200).send({ count: result });
+        });
+    
+        app.get("/foods/:id", async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await foodsCollection.findOne(query);
+          res.send(result);
+        });
+    
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+          "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+      } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+      }
+    }
+    run().catch(console.dir);
+    
+    app.get("/", (req, res) => {
+      res.send("Restaurent website is running");
+    });
+    
+    app.listen(port, () => {
+      console.log(
+        `Restaurent management website server is running on port: ${port}`
+      );
+    });
+    
